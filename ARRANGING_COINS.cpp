630a2bdeefb6393392a4eb815fac7a5b340d309e
Raj Keshri <107674465+rajkeshri2318@users.noreply.github.com>
2022-12-21 10:04:54 +0530

    Create ARRANGING_COINS.cpp

---
+++ ARRANGING_COINS.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        int cnt = 0;
+        for(int i =1; i<=n; i++) {
+            if((n-i)<0) break;
+            cnt++;
+            n = n-i;
+        }
+        return cnt;
+    }
+};
